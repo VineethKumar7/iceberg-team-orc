63ef1bfecb9672de59cacb422ed6411bf4b11bfc
suman <sumanrbt1997@gmail.com>
2024-05-07 10:42:53 +0530

    updates with initial results with sorting physical optimization

--- physical_optimization.py
+++ physical_optimization.py
@@ -1,8 +1,12 @@
+import time
+
+import duckdb
 import pyarrow as pa
 from pyarrow import orc
 import pyarrow.orc as orc
 from datetime import datetime
 import os
+import pandas as pd
 
 ware_house_path = 'warehouse/default/lineitem/data/'
 for dirs, subdirs, files in os.walk(ware_house_path):
@@ -22,6 +26,33 @@ def find_min_max_date(file_path: str, column: str='l_shipdate'):
     return min(dates), max(dates)
 
 if __name__ == '__main__':
-    for file in orc_files:
+    query_results = []
+    st_time = time.time()
+    for i, file in enumerate(orc_files):
         min_date, max_date = find_min_max_date(file_path=file, column='l_shipdate')
-        print(min_date, max_date)
+        print(i, min_date, max_date)
+        if max_date < pd.to_datetime("1994-01-01") or min_date > pd.to_datetime("1995-01-01"):
+            pass
+        else:
+            df = pd.read_orc(file)
+
+            lineitem_table = f"""
+            CREATE TABLE IF NOT EXISTS lineitem{i} AS SELECT * FROM df
+            """
+            query6 = f"""
+                    select
+                      sum(l_extendedprice * l_discount) as revenue
+                    from
+                      lineitem{i}
+                    where
+                      l_shipdate >= date '1994-01-01'
+                      and l_shipdate < date '1994-01-01' + interval '1' year
+                      and l_discount between 0.06 - 0.01 and 0.06 + 0.01
+                      and l_quantity < 24
+                    """
+            duckdb.query(lineitem_table)
+            result = duckdb.query(query6)
+            query_results.append(result.fetchone()[0])
+    print(f"{query_results = }")
+    print(f"{sum(query_results) = }")
+    print(f"time taken for optimized query: {time.time() - st_time}")
--- results.md
+++ results.md
@@ -3,9 +3,11 @@ from x86_64 Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz (ubuntu 20.04.1)
 
 (**not** avg running)
 
-| #                                | query3 | query6 |
-|----------------------------------|--------|--------|
-| duckdb with no processing at all | 0.05   | 0.0005 |
-| using iceberg and orc format     | 9.6    | 1.46   |
-| Physical Optimisation1 - sorting |        |        |
-| Physical Optimisation2 -         |        |        |
+| #                                         | query3   | query6 |
+|-------------------------------------------|----------|--------|
+| duckdb with no processing at all          | 0.05     | 0.0005 |
+| using iceberg and orc format (spark)      | 9.6      | 1.46   |
+| Physical Optimisation1 - sorting (duckdb) | not done | 40.92  |
+| Physical Optimisation2 -                  |          |        |
+
+Note
